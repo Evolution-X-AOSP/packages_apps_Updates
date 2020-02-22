4189ff25604356af30672590a978b26eaf8a7a42
hritikutekar <hritikdrocks@gmail.com>
2020-02-22 18:20:08 +0530

    Update the timestamp on manual update check

--- src/org/pixelexperience/ota/UpdatesActivity.java
+++ src/org/pixelexperience/ota/UpdatesActivity.java
@@ -264,6 +264,10 @@ public class UpdatesActivity extends UpdatesListActivity {
             editor.putString("LastUpdateCheck", date);
             editor.apply();
 
+            LastUpdateCheck = sharedPref.getString("LastUpdateCheck", "Not checked");
+            lastUpdateCheck.setText(String.format(getResources()
+                    .getString(R.string.last_successful_check_for_update), LastUpdateCheck));
+
             showSnackbar(
                     updateAvailable ? R.string.update_found_notification : R.string.snack_no_updates_found,
                     Snackbar.LENGTH_SHORT);
