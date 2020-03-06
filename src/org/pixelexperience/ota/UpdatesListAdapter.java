77881e89b88789b1568f4253521a1c364cd6ca8f
hritikutekar <hritikdrocks@gmail.com>
2020-03-06 08:22:00 +0530

    Check for the download medium every time app launches.

--- src/org/pixelexperience/ota/UpdatesListAdapter.java
+++ src/org/pixelexperience/ota/UpdatesListAdapter.java
@@ -156,6 +156,12 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
         viewHolder.mProgressBar.setVisibility(View.VISIBLE);
         viewHolder.mProgressText.setVisibility(View.VISIBLE);
         viewHolder.mBuildSize.setVisibility(View.VISIBLE);
+
+        if (Utils.isOnWifiOrEthernet(mActivity)) {
+            mActivity.findViewById(R.id.mobile_data_warning).setVisibility(View.GONE);
+        } else {
+            mActivity.findViewById(R.id.mobile_data_warning).setVisibility(View.VISIBLE);
+        }
     }
 
     private void handleNotActiveStatus(ViewHolder viewHolder, UpdateInfo update) {
@@ -190,6 +196,12 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
         viewHolder.mProgressBar.setVisibility(View.INVISIBLE);
         viewHolder.mProgressText.setVisibility(View.INVISIBLE);
         viewHolder.mBuildSize.setVisibility(View.VISIBLE);
+
+        if (Utils.isOnWifiOrEthernet(mActivity)) {
+            mActivity.findViewById(R.id.mobile_data_warning).setVisibility(View.GONE);
+        } else {
+            mActivity.findViewById(R.id.mobile_data_warning).setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
